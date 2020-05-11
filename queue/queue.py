3bdf3ddaabac1b19e4d500bd0929178a550de870
igor <embiggenerd@gmail.com>
2020-05-11 18:35:49 -0400

    queue works with linked list

--- queue/queue.py
+++ queue/queue.py
@@ -1,3 +1,5 @@
+from singly_linked_list import LinkedList
+
 """
 A queue is a data structure whose primary purpose is to store and
 return elements in First In First Out order. 
@@ -10,27 +12,52 @@ return elements in First In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Queue?
 """
+# class Queue:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+    
+#     def __len__(self):
+#         return len(self.storage)
+
+#     def enqueue(self, value):
+#         self.size = self.size + 1
+#         self.storage.insert(0, value)
+
+#     def dequeue(self):
+#         if self.size == 0:
+#             return None
+#         else :
+#             self.size = self.size - 1
+#             return self.storage.pop()
+
+#     def state_props(self):
+#         print(self.size, self.storage)
+linked = LinkedList()
 class Queue:
     def __init__(self):
         self.size = 0
-        self.storage = []
+        self.storage = linked
     
     def __len__(self):
-        return len(self.storage)
+        return self.size
 
     def enqueue(self, value):
         self.size = self.size + 1
-        self.storage.insert(0, value)
+        # self.storage.insert(0, value)
+        self.storage.add_to_tail(value)
 
     def dequeue(self):
         if self.size == 0:
             return None
         else :
             self.size = self.size - 1
-            return self.storage.pop()
+            # return self.storage.pop()
+            return self.storage.remove_from_head()
 
     def state_props(self):
         print(self.size, self.storage)
+
 
 q = Queue()
 
