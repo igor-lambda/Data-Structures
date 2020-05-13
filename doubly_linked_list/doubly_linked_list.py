a9166a32975d07cbb55871167715e0598d1f6609
igor <embiggenerd@gmail.com>
2020-05-13 13:58:44 -0400

    move_to_end passing

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -129,6 +129,7 @@ class DoublyLinkedList:
                 current_node.next.prev == current_node.prev
                 current_node.prev.next = current_node.next
                 self.head.prev = node
+                node.next = self.head
                 self.head = node
             current_node = current_node.next
 
@@ -136,7 +137,18 @@ class DoublyLinkedList:
     """Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List."""
     def move_to_end(self, node):
-        pass
+        if self.is_empty():
+            return None
+        current_node = self.head
+        while current_node is not None:
+            if current_node == node:
+                current_node.next.prev == current_node.prev
+                current_node.prev.next = current_node.next
+                self.tail.next = node
+                node.prev = self.tail
+                self.tail = node
+            current_node = current_node.next
+
 
     """Removes a node from the list and handles cases where
     the node was the head or the tail"""
